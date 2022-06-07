f5f0ed71b91883573a67916fd1e6a0d3a42a2e16
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-07 15:48:50 +0200

    Refactoring js coding

--- milestone-3/js/script.js
+++ milestone-3/js/script.js
@@ -2,21 +2,20 @@
 for (let i = 1; i <= 100; i++) {
   let numberText = i;
   // Dichiaro la variabile per la creazione del div square
-  const square = document.createElement("div");
-  square.classList.add("number-square");
+  const numberSquare = document.createElement("div");
+  numberSquare.classList.add("number-square");
   // Imposto l'if statement
   if (i % 3 === 0 && i % 5 === 0) {
     numberText = "FizzBuzz";
-    square.classList.add("fizz-buzz");
+    numberSquare.classList.add("fizz-buzz");
   } else if (i % 3 === 0) {
     numberText = "Fizz";
-    square.classList.add("fizz");
+    numberSquare.classList.add("fizz");
   } else if (i % 5 === 0) {
     numberText = "Buzz";
-    square.classList.add("buzz");
+    numberSquare.classList.add("buzz");
   }
   //   Aggiungo il testo al div square
-  const numberSquare = square;
   numberSquare.innerHTML = `${numberText}`;
   // Stampo in pagina aggiungendo il div square alla grid
   const numbersGrid = document.getElementById("numbers-grid");
